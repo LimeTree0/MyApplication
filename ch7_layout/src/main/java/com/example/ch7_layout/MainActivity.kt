6347fdd7235caadc57c1c259cdc3c7c6a43406c7
영범 홍 <bum9516@gmail.com>
2022-07-13 15:22:32 +0900

    안드로이드 전화번호 창

--- ch7_layout/src/main/java/com/example/ch7_layout/MainActivity.kt
+++ ch7_layout/src/main/java/com/example/ch7_layout/MainActivity.kt
@@ -1,14 +1,11 @@
-package com.example.myapplication
+package com.example.ch7_layout
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
 }
