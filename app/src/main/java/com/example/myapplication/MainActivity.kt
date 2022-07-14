4bd54f7c953827935bf6671afd3a3737ecd2e213
영범 홍 <bum9516@gmail.com>
2022-07-14 13:53:03 +0900

    안드로이드 메신저 화면

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,58 +1,15 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.util.Log
-import android.view.KeyEvent
-import android.view.MotionEvent
-import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.ActivityMain2Binding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.checkbox.setOnCheckedChangeListener(object :
-            CompoundButton.OnCheckedChangeListener {
-            override fun onCheckedChanged(p0: CompoundButton?, p1: Boolean) {
-                Log.d("kkang", "체크박스 클릭")
-            }
-            })
-
-        binding.button.setOnClickListener {
-            Log.d("kkang", "클릭 이벤트")
-        }
-        binding.button.setOnLongClickListener {
-            Log.d("kkang", "롱클릭 이벤트")
-            true
-        }
-
     }
-
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        when (event?.action){
-            MotionEvent.ACTION_DOWN->{
-                Log.d("kkang", "Touch down event")
-            }
-            MotionEvent.ACTION_UP->{
-                Log.d("kkang", "Touch up event")
-            }
-        }
-        return super.onTouchEvent(event)
-    }
-
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        when(keyCode){
-            KeyEvent.KEYCODE_VOLUME_UP->Log.d("kkang", "Back button을 눌렀네요")
-            KeyEvent.KEYCODE_VOLUME_DOWN->Log.d("kkang", "Volume up button을 눌렀네요")
-            KeyEvent.KEYCODE_BACK->Log.d("kkang", "Volume down button을 눌렀네요")
-        }
-        return super.onKeyDown(keyCode, event)
-    }
-
-
-
 }
