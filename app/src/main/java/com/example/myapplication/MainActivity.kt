857bdd5c3355c0590c586ea48991b6e6af74f168
영범 홍 <bum9516@gmail.com>
2022-07-14 20:37:02 +0900

    안드로이드 알람

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,79 @@
 package com.example.myapplication
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.graphics.Color
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.databinding.ActivityMain2Binding
+import androidx.core.app.NotificationCompat
+import com.example.myapplication.databinding.DialogInputBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMain2Binding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val dialogBinding = DialogInputBinding.inflate(layoutInflater)
+        setContentView(dialogBinding.root)
 
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val builder: NotificationCompat.Builder
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "one-channel"
+            val channelName = "My Channel One"
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            channel.description = "My Channel One Description"
+            channel.setShowBadge(true)
+            val uri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+            val audioAttributes = AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .build()
+            channel.setSound(uri, audioAttributes)
+            channel.enableLights(true)
+            channel.lightColor = Color.RED
+            channel.enableVibration(true)
+            channel.vibrationPattern = longArrayOf(100,200,100,200)
+
+            manager.createNotificationChannel(channel)
+
+            builder = NotificationCompat.Builder(this, channelId)
+        }
+        else {
+            builder = NotificationCompat.Builder(this)
+        }
+
+        builder.setSmallIcon(android.R.drawable.ic_notification_overlay)
+        builder.setWhen(System.currentTimeMillis())
+        builder.setContentTitle("Content Title")
+        builder.setContentText("Content Massage")
+        /*builder.setContentIntent(pendingIntent)*/
+        
+        /*val actionPendingIntent = Intent(this, OneReceiver::class.java)
+        val intent = Intent(this, dialogBinding::class.java)
+        val pendingIntent =
+            PendingIntent.getActivity(this, 10, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT)
+        builder.addAction(
+            NotificationCompat.Action.Builder(
+                android.R.drawable.stat_notify_more,
+                "Action",
+                actionPendingIntent
+            )
+        )*/
+
+
+
+        manager.notify(11, builder.build())
     }
 }
